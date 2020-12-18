e49362c16c5d30abd3ff0d27a2e46d501963b4fc
Raimonds <maijs@users.noreply.github.com>
2020-12-18 17:47:52 +0200

    Indexing status for entity is removed when entity document is removed from the index

--- src/EventSubscriber/IndexingStatus.php
+++ src/EventSubscriber/IndexingStatus.php
@@ -73,6 +73,12 @@ class IndexingStatus implements EventSubscriberInterface {
         $this->indexingStatusOperationManager->setSuccessIndexingStatus($request_wrapper->getObject(), $request_wrapper->getPluginInstance());
       }
     }
+    elseif ($operation == ElasticsearchOperations::DOCUMENT_DELETE) {
+      if ($this->isDeleteResultSuccessful($result)) {
+        // Delete indexing status on removal from index.
+        $this->indexingStatusOperationManager->deleteIndexingStatus($request_wrapper->getObject(), $request_wrapper->getPluginInstance());
+      }
+    }
   }
 
   /**
@@ -86,6 +92,19 @@ class IndexingStatus implements EventSubscriberInterface {
     $body = $result->getResultBody();
 
     return isset($body['result']) && in_array($body['result'], ['created', 'updated']);
+  }
+
+  /**
+   * Returns TRUE if document delete result is successful.
+   *
+   * @param \Drupal\elasticsearch_helper\ElasticsearchRequestResultInterface $result
+   *
+   * @return bool
+   */
+  protected function isDeleteResultSuccessful(ElasticsearchRequestResultInterface $result) {
+    $body = $result->getResultBody();
+
+    return isset($body['result']) && $body['result'] == 'deleted';
   }
 
 }
