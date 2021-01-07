94e34218509fd9d060149666fe5a446a97432d59
Raimonds Kalnins <raimonds.kalnins@gmail.com>
2021-01-07 20:27:30 +0200

    Only bare indexing status table fields are added to views data.

--- src/ViewsData.php
+++ src/ViewsData.php
@@ -2,37 +2,10 @@
 
 namespace Drupal\elasticsearch_helper_index_management;
 
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 /**
  * Views data provider for index management module.
  */
-class ViewsData implements ContainerInjectionInterface {
-
-  /**
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * ViewsData constructor.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
-    $this->entityTypeManager = $entity_type_manager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('entity_type.manager')
-    );
-  }
+class ViewsData {
 
   /**
    * Provides views data for index management tables.
@@ -40,7 +13,7 @@ class ViewsData implements ContainerInjectionInterface {
   public function getViewsData() {
     $data = [];
 
-    $data['elasticsearch_helper_indexing_status']['table']['group'] = t('Elasticsearch indexing status');
+    $data['elasticsearch_helper_indexing_status']['table']['group'] = t('Elasticsearch indexing');
 
     $data['elasticsearch_helper_indexing_status']['table']['base'] = [
       'field' => 'sid',
@@ -48,30 +21,6 @@ class ViewsData implements ContainerInjectionInterface {
       'help' => t('Elasticsearch indexing status table.'),
       'query_id' => 'views_query',
     ];
-
-    foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
-      $base_table = $entity_type->getDataTable() ?: $entity_type->getBaseTable();
-      $id_key = $entity_type->getKey('id');
-
-      $data[$base_table]['elasticsearch_helper_indexing_status'] = [
-        'title' => t('Indexing status'),
-        'relationship' => [
-          'group' => t('Elasticsearch Helper'),
-          'label' => t('Indexing status'),
-          'help' => t('Indexing status'),
-          'base' => 'elasticsearch_helper_indexing_status',
-          'base field' => 'id',
-          'relationship field' => $id_key,
-          'id' => 'standard',
-          'extra' => [
-            [
-              'field' => 'entity_type',
-              'value' => $entity_type_id,
-            ],
-          ],
-        ],
-      ];
-    }
 
     $data['elasticsearch_helper_indexing_status']['sid'] = [
       'title' => t('Status entry ID'),
